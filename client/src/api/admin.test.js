d4796084299172e3155686fd743653ee4b4fd1c4
Vinicius-Muniz <vs454595@gmail.com>
2025-01-17 10:10:18 -0300

    Adicionando novos casos de teste

--- client/src/api/admin.test.js
+++ client/src/api/admin.test.js
@@ -20,6 +20,7 @@ jest.mock('./client', () => {
   };
 });
 
+/* ******************************* Testes já presentes no projeto ******************************* */
 describe('admin API', () => {
   describe('getPageChildren', () => {
     it('works', () => {
@@ -50,6 +51,64 @@ describe('admin API', () => {
       );
     });
   });
+  /* ******************************** Novos Testes Criados ******************************** */
+
+  describe('getPageChildren - Conditional Validations', () => {
+    it('#fields and onlyWithChildren (both defined -> true)', () => {
+      const result = getPageChildren(7, { fields: ['title'], onlyWithChildren: true });
+      expect(client.get).toBeCalledWith(
+        `${ADMIN_API.PAGES}?child_of=7&for_explorer=1&fields=parent,title&has_children=1`,
+      );
+      expect(result).toBeTruthy();
+    });
+  
+    it('#fields and onlyWithChildren (fields not defined -> false)', () => {
+      const result = getPageChildren(7, { onlyWithChildren: true });
+      expect(result).toBeFalsy();
+    });
+  
+    it('#fields and onlyWithChildren (onlyWithChildren not defined -> false)', () => {
+      const result = getPageChildren(7, { fields: ['title'] });
+      expect(result).toBeFalsy();
+    });
+  
+    it('#fields and offset (both defined -> true)', () => {
+      const result = getPageChildren(8, { fields: ['title'], offset: 20 });
+      expect(client.get).toBeCalledWith(
+        `${ADMIN_API.PAGES}?child_of=8&for_explorer=1&fields=parent,title&offset=20`,
+      );
+      expect(result).toBeTruthy();
+    });
+  
+    it('#fields and offset (fields not defined -> false)', () => {
+      const result = getPageChildren(8, { offset: 20 });
+      expect(result).toBeFalsy();
+    });
+  
+    it('#fields and offset (offset not defined -> false)', () => {
+      const result = getPageChildren(8, { fields: ['title'] });
+      expect(result).toBeFalsy();
+    });
+  
+    it('#onlyWithChildren and offset (both defined -> true)', () => {
+      const result = getPageChildren(9, { onlyWithChildren: true, offset: 30 });
+      expect(client.get).toBeCalledWith(
+        `${ADMIN_API.PAGES}?child_of=9&for_explorer=1&fields=parent&has_children=1&offset=30`,
+      );
+      expect(result).toBeTruthy();
+    });
+  
+    it('#onlyWithChildren and offset (onlyWithChildren not defined -> false)', () => {
+      const result = getPageChildren(9, { offset: 30 });
+      expect(result).toBeFalsy();
+    });
+  
+    it('#onlyWithChildren and offset (offset not defined -> false)', () => {
+      const result = getPageChildren(9, { onlyWithChildren: true });
+      expect(result).toBeFalsy();
+    });
+  });
+  /* ******************************** Fim dos novos testes criados ********************************** */
 
   describe('getPage', () => {
     it('should return a result by with a default id argument', () => {
